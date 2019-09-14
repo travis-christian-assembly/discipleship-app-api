b5c039b24a2edf9c249c05a052379c2f66e4205b
unkdrew <drewwei0923@gmail.com>
2019-09-14 14:07:18 -0500

    Added API GetCourse

---
+++ get_course.js
@@ -0,0 +1,22 @@
+import * as dynamoDb from "./lib/dynamodb";
+import { success, notFoundError, failure } from "./lib/response";
+
+export async function main(event, context) {
+  const params = {
+    TableName: "Courses",
+    Key: {
+      CourseId: event.pathParameters.courseId
+    }
+  };
+
+  try {
+    const result = await dynamoDb.call("get", params);
+    if (result.Item) {
+      return success(result.Item);
+    } else {
+      return notFoundError({ status: false, error: "Item not found." });
+    }
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
